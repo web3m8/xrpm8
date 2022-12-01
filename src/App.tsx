2512c00d5f229c64da550092689156a3332725b4
S A Sureshkumar <me@ssureshkumar.com>
2022-12-01 11:57:19 +0530

    update titile

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import {FC} from "react";
 
 const App: FC = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            XRP M8
-        </h1>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                XRP Mate - XRPM8
+            </h1>
+            <p>XRP NFT Marketplace, Buy, Sell and Collect NFTs on XRP Ledger</p>
+        </>
     );
 }
 
