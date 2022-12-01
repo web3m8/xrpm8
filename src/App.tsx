3c063f4e6477e71585365cc1c4073b502048c3ba
S A Sureshkumar <me@ssureshkumar.com>
2022-12-01 11:32:04 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const App: FC = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            XRP M8
+        </h1>
+    );
+}
+
+export default App;
